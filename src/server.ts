66d555f3f63824d3dcc543bf3f0ddadbc1456e99
Carlos Vaz <carlosfelipe_2002@outlook.com>
2023-06-07 18:25:16 -0300

    express setup

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,8 @@
-import { Router } from 'express';
+import express, { Express, Request, Response } from 'express';
 
-const router = Router();
-router.get('/', (req, res) => console.log('Hello World'));
+const app: Express = express();
+const port = 8080;
+
+app.get('/', (req: Request, res: Response) => console.log('Hello World'));
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
